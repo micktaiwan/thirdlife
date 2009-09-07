c12d17b6aaff9534c20e0d8525effa65f391edb2
Mickael Faivre-Maçon <faivrem@gmail.com>
2009-09-07 04:07:37 +0200

    cleaned up welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   
   layout 'general'
+  include AuthenticatedSystem
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
